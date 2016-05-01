349f4828b9e1a6b8c623a51fc9b040a6e30f492e
Scott Huffman <canderis.ordo@gmail.com>
2016-05-01 16:36:09 -0500

    Working Ajax Request function and file.

--- Server Connection Test/ViewController.swift
+++ Server Connection Test/ViewController.swift
@@ -24,10 +24,15 @@ class ViewController: UIViewController {
     }
 
     @IBAction func establishConnection(sender: UIButton) {
-        
         ajaxRequest(
-            ["data":dataToSend.text!],
-            url: "http://dev.canderis.com/ServerConnectionTest/conntection.php",
+            [
+                "one": dataToSend.text!,
+                "two": "2",
+                "three": "testing",
+                "four": "blah",
+            
+            ],
+            url: "connection.php",
             success: { (response) -> () in
                 print(response)
                 self.serverOutput.text = "\(response["data"])"
@@ -36,132 +41,6 @@ class ViewController: UIViewController {
                 self.serverOutput.text = "errorString = \(error)"
             }
         )
-        
-        /*
-        let myUrl = NSURL(string: "http://dev.canderis.com/ServerConnectionTest/conntection.php");
-        let request = NSMutableURLRequest(URL:myUrl!);
-        request.HTTPMethod = "POST";
-        
-        // Compose a query string
-        let postString = "data=\(dataToSend.text!)";
-        
-        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
-        
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
-            data, response, error in
-            
-            var textField = ""
-
-            if error != nil {
-                textField = "error=\(error)"
-                return
-            }
-            
-            // You can print out response object
-            //print("response = \(response)")
-            
-            // Print out response body
-            //let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
-            //print("responseString = \(responseString)")
-            
-            //Let’s convert response sent from a server side script to a NSDictionary object:
-            
-            do {
-                let myJSON = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as! [String: AnyObject]
-                if let text = myJSON["data"] {
-                    textField = text as! String
-                }
-            } catch let error as NSError {
-                textField = "Failed to load: \(error.localizedDescription)"
-            }
-            dispatch_async(dispatch_get_main_queue(), {
-                self.serverOutput.text = textField
-            })
-
-            
-        }
-        task.resume()
-        */
     }
     
-    func ajaxRequest( params: Dictionary<String, String>, url: String, success: (([String:AnyObject])->())? = nil, failure: ((String)->())? = nil ) {
-        
-        let nsurl = NSURL(string: url)
-        
-        do {
-            print(params)
-            let jsonData = try NSJSONSerialization.dataWithJSONObject(params, options: NSJSONWritingOptions.PrettyPrinted)
-            print(jsonData)
-            
-            // create post request
-            let request = NSMutableURLRequest(URL: nsurl!)
-            request.HTTPMethod = "POST"
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-            do {
-                let decoded = try NSJSONSerialization.JSONObjectWithData(jsonData, options: []) as? [String:String]
-                // here "decoded" is the dictionary decoded from JSON data
-                print(decoded)
-            } catch let error as NSError {
-                print(error)
-            }
-            
-            print (request)
-            // insert json data to the request
-            request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
-            request.HTTPBody = jsonData
-            
-            print(request)
-            
-            let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
-                if error != nil{
-                    if failure == nil{
-                        print("Error -> \(error)")
-
-                    }
-                    else {
-                        dispatch_async(dispatch_get_main_queue(), {
-                            failure!("\(error)")
-                        })
-
-                    }
-                    return
-                }
-                
-                do {
-                    let result = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? [String:AnyObject]
-                    
-                    dispatch_async(dispatch_get_main_queue(), {
-                        success?(result!)
-                    })
-                    
-                } catch {
-                    if failure == nil{
-                        print("Error -> \(error)")
-                    }
-                    else {
-                        dispatch_async(dispatch_get_main_queue(), {
-                            failure!("\(error)")
-                        })
-                    }
-                    return
-                }
-            }
-            
-            task.resume()
-            
-        }
-        catch {
-            if failure == nil{
-                print("Error -> \(error)")
-            }
-            else {
-                dispatch_async(dispatch_get_main_queue(), {
-                    failure!("\(error)")
-                })
-            }
-        }
-    }
-
 }
-
